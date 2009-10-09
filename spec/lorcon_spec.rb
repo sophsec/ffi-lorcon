af7e2a56adc937ef1575bbe3e5031ca7c392b9f8
postmodern <postmodern.mod3@gmail.com>
2009-10-08 19:49:53 -0700

    Updated specs.

--- spec/lorcon_spec.rb
+++ spec/lorcon_spec.rb
@@ -1,4 +1,4 @@
-require 'lorcon-ffi/ffi'
+require 'lorcon-ffi/lorcon'
 require 'lorcon-ffi/version'
 
 require 'spec_helper'
@@ -13,16 +13,16 @@ describe Lorcon do
   end
 
   it "should list the available drivers" do
-    driver = Lorcon.list_drivers
+    drivers = Lorcon.list_drivers
 
-    driver.should_not be_nil
-    driver.name.should_not be_empty
+    drivers.should_not be_empty
   end
 
   it "should find the driver with a specified name" do
     driver = Lorcon.find_driver('mac80211')
 
     driver.should_not be_nil
+    driver.should_not be_null
     driver.name.should == 'mac80211'
   end
 
@@ -30,6 +30,7 @@ describe Lorcon do
     driver = Lorcon.default_driver(IFACE)
 
     driver.should_not be_nil
+    driver.should_not be_null
     driver.name.should_not be_empty
   end
 end
